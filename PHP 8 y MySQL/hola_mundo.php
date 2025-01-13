9766f9211fb12f2c76a8cdd5711fb0884fe92e80
WalterNickSuelArroyo <nicodehero21@outlook.com>
2025-01-13 13:38:27 -0500

    hello-world-php

---
+++ PHP 8 y MySQL/hola_mundo.php
@@ -0,0 +1,28 @@
+<?php
+// echo nos sirve para mostrar información en pantalla
+
+# Otro comentario
+
+/* Otro comentario
+mas largo
+*/
+
+echo "Hola mundo";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo "Hola mundo php, dentro de html"; ?>
+    </h1>
+</body>
+
+</html>
